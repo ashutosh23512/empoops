e661a0348738180220d73a5e7d98f30e685a0cf2
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-28 13:23:36 +0530

    [Ashutosh]Added:Check Part Time

--- src/com/empoops/EmpOops.java
+++ src/com/empoops/EmpOops.java
@@ -2,27 +2,28 @@ package com.empoops;
 import java.util.Random;
 
 public class EmpOops {
-
-	public static void main(String[] args)
-	{
-		 int IS_FULL_TIME = 1;
-		  int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	
+	public static void main(String[] args) {
 		
-		
-			
-	     int empHrs = 0, empWage = 0;
-	     
-	     Random r = new Random();
-	     int empCheck =  r.nextInt(2) ;
-	     
-	     if(empCheck == IS_FULL_TIME)
-	             empHrs = 8;
-	     else
-	    	     empHrs = 0;
-	     
-	     empWage = empHrs * EMP_RATE_PER_HOUR;
-	     System.out.println("Emp Wage: " + empWage);
-		}
+     int empHrs = 0, empWage = 0;
+     
+     Random r = new Random();
+     int empCheck =  r.nextInt(3) ;
+     
+     if(empCheck == IS_FULL_TIME)
+             empHrs = 8;
+     else if (empCheck == IS_PART_TIME)
+             empHrs = 4;
+     else
+    	     empHrs = 0;
+     
+     
+     empWage = empHrs * EMP_RATE_PER_HOUR;
+     System.out.println("Emp Wage: " + empWage);
+	}
 
 	
 }
