532c4301874253348c512973d2b1debbf4cb5694
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-28 13:51:48 +0530

    [Ashutosh]Added:Using Interface

--- src/com/empoops/EmpOops.java
+++ src/com/empoops/EmpOops.java
@@ -1,23 +1,24 @@
 package com.empoops;
 import java.util.Random;
 
-public class EmpOops {
-
+public class EmpOops implements IComputeEmpWage{
+	
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
    
 	public int numOfCompany = 0;
 	private CompanyEmpWage[] companyEmpWageArray;
 	
-    public  EmpOops() {
+    public EmpOops() {
     	companyEmpWageArray = new CompanyEmpWage[5];
     }
-    private void addCompanyEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth ) {
+    @Override
+    public void addCompanyEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth ) {
     	companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
     numOfCompany++;
     }
-    
-	private void computeEmpWage() {
+    @Override
+	public void computeEmpWage() {
 		for(int i =0; i < numOfCompany; i++) {
 			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
 		System.out.println(companyEmpWageArray[i]);
@@ -49,9 +50,16 @@ public class EmpOops {
        return totalEmpHours * companyEmpWage.empRatePerHour;
 
 	}
+	
+	@Override
+	public int getTotalWage(String company) {
+		return 0;
+	}
 	 public static void main(String[] args) {
 		 EmpOops empWageBuilder = new EmpOops();
-		 empWageBuilder.addCompanyEmpWage("Reliance",  20,  2,  10);
+		 empWageBuilder.addCompanyEmpWage("Reliance",  20,  5,  10);
 		 empWageBuilder.addCompanyEmpWage("BigBazar",  10,  4,  20);
 		 empWageBuilder.computeEmpWage();
-	 }}
+	 }
+	
+}
