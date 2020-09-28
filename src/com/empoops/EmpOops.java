7206d33de213cd86ac109e1e8f8a9f467ecb8e60
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-28 13:36:31 +0530

    [Ashutosh]Added:Using Method

--- src/com/empoops/EmpOops.java
+++ src/com/empoops/EmpOops.java
@@ -2,37 +2,41 @@ package com.empoops;
 import java.util.Random;
 
 public class EmpOops {
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
+	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
+
+	public static int computeEmpWage() {
+		int empHrs = 0, totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
+
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		totalWorkingDays++;
+
+		Random r = new Random();
+		int empCheck = r.nextInt(3);
+
+		switch (empCheck) {
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		default:
+			empHrs = 0;
+
+		}
+		totalEmpHrs += empHrs;
+		System.out.println("Day : " + totalWorkingDays + "empHrs : " + empHrs);
+		}
+		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+		return totalEmpWage;
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     int empHrs = 0, totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
-     
-     while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-    	 totalWorkingDays++;
-    	 
-     Random r = new Random();
-     int empCheck =  r.nextInt(3) ;
-     
-     switch(empCheck) {
-     case IS_FULL_TIME:
-             empHrs = 8;
-             break;
-     case IS_PART_TIME:
-             empHrs = 4;
-             break;
-     default:
-    	     empHrs = 0;
-     
-     }
-     totalEmpHrs += empHrs;
-     System.out.println("Day : " + totalWorkingDays + "empHrs : " + empHrs);
-	}
-      totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-     System.out.println("Total Emp Wage: " + totalEmpWage);
+    computeEmpWage();
 	}
 }
